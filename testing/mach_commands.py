eb744c90b5ae9d86392970dda70f3a7c5b61e49b
the_r3dacted <k4sum1@eclipse.cx>
2026-05-23 10:51:21 -0700

    Revert "Issue #1845 - Remove Marionette files"
    This reverts commit 63b8aaa49509ef54628061db640bc0afdf0b5a1a.

--- testing/mach_commands.py
+++ testing/mach_commands.py
@@ -56,6 +56,16 @@ TEST_SUITES = {
         'mach_command': 'crashtest',
         'kwargs': {'test_file': None},
     },
+    'firefox-ui-functional': {
+        'aliases': ('Fxfn',),
+        'mach_command': 'firefox-ui-functional',
+        'kwargs': {},
+    },
+    'firefox-ui-update': {
+        'aliases': ('Fxup',),
+        'mach_command': 'firefox-ui-update',
+        'kwargs': {},
+    },
     'jetpack': {
         'aliases': ('J',),
         'mach_command': 'jetpack-test',
@@ -128,6 +138,18 @@ TEST_FLAVORS = {
     'chrome': {
         'mach_command': 'mochitest',
         'kwargs': {'flavor': 'chrome', 'test_paths': []},
+    },
+    'firefox-ui-functional': {
+        'mach_command': 'firefox-ui-functional',
+        'kwargs': {'tests': []},
+    },
+    'firefox-ui-update': {
+        'mach_command': 'firefox-ui-update',
+        'kwargs': {'tests': []},
+    },
+    'marionette': {
+        'mach_command': 'marionette-test',
+        'kwargs': {'tests': []},
     },
     'mochitest': {
         'mach_command': 'mochitest',
