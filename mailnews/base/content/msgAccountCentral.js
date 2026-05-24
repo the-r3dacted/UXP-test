635ec5203537220b70d380d020f4da3543d0cf9c
the_r3dacted <k4sum1@eclipse.cx>
2026-05-24 12:23:23 -0700

    Revert "[MailNews] Disable leftover Account Provisioner code."
    This reverts commit 9733f9d2e53a1580282f54356ba15c5c1dbcbbe2.

--- mailnews/base/content/msgAccountCentral.js
+++ mailnews/base/content/msgAccountCentral.js
@@ -287,11 +287,9 @@ function CreateNewAccount()
 function CreateNewAccountTB(type)
 {
   if (type == "mail") {
-#ifndef ATH_EPYRUS
     if (Services.prefs.getBoolPref("mail.provider.enabled"))
         NewMailAccountProvisioner(null);
     else
-#endif
         AddMailAccount();
     return;
   }
