412d07272e5e2033aee9d2cbf7f4d7c1cb90bb96
the_r3dacted <k4sum1@eclipse.cx>
2026-05-24 10:43:00 -0700

    Revert "Remove XPCOM checks for Windows Vista and clean up unused header files."
    This reverts commit 13a9ea6486df49cee94a5e5a23b02dca13968eb7.

--- xpcom/build/XPCOMInit.cpp
+++ xpcom/build/XPCOMInit.cpp
@@ -110,6 +110,7 @@ extern nsresult nsStringInputStreamConstructor(nsISupports*, REFNSIID, void**);
 #include "SpecialSystemDirectory.h"
 
 #if defined(XP_WIN)
+#include "mozilla/WindowsVersion.h"
 #include "nsWindowsRegKey.h"
 #endif
 
