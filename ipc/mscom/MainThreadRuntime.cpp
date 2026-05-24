7967f84b929c2244388747758a5f2122f547d6b3
the_r3dacted <k4sum1@eclipse.cx>
2026-05-24 11:36:45 -0700

    Revert "Remove more obsolete checks in dom/media and ipc."
    This reverts commit 5d1eb0d3aa590338930553cb7e09c43c4840ef1a.

--- ipc/mscom/MainThreadRuntime.cpp
+++ ipc/mscom/MainThreadRuntime.cpp
@@ -66,8 +66,11 @@ MainThreadRuntime::MainThreadRuntime()
     return;
   }
 
-  // Use the strictest policy available.
-  ULONG_PTR exceptionSetting = COMGLB_EXCEPTION_DONOT_HANDLE_ANY;
+  // Windows 7 has a policy that is even more strict. We should use that one
+  // whenever possible.
+  ULONG_PTR exceptionSetting = IsWin7OrLater() ?
+                               COMGLB_EXCEPTION_DONOT_HANDLE_ANY :
+                               COMGLB_EXCEPTION_DONOT_HANDLE;
   mInitResult = globalOpts->Set(COMGLB_EXCEPTION_HANDLING, exceptionSetting);
   MOZ_ASSERT(SUCCEEDED(mInitResult));
 }
