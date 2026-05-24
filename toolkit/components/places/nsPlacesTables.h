410fd0f0164f682a08ee0635c3123f3fffd1b601
the_r3dacted <k4sum1@eclipse.cx>
2026-05-24 10:04:34 -0700

    Revert "Issue #756 - Remove Contextual Identity from UnifiedComplete"
    This reverts commit 2d20c38f8395871facc2b23058770156c30611f5.

--- toolkit/components/places/nsPlacesTables.h
+++ toolkit/components/places/nsPlacesTables.h
@@ -132,8 +132,10 @@
 //       nsPlacesAutoComplete.js.
 #define CREATE_MOZ_OPENPAGES_TEMP NS_LITERAL_CSTRING( \
   "CREATE TEMP TABLE moz_openpages_temp (" \
-    "  url TEXT PRIMARY KEY" \
+    "  url TEXT" \
+    ", userContextId INTEGER" \
     ", open_count INTEGER" \
+    ", PRIMARY KEY (url, userContextId)" \
   ")" \
 )
 
